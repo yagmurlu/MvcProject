0277ea151dc42ca193a1c3e8fbf404a8ef2f148e
yagmurlu <mavibulut0106@gmail.com>
2021-05-21 22:40:04 +0300

    Writer Add-migration

---
+++ BussinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.ValidationRules
+{
+    public class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()//condtuctor method
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçilemez!");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadı boş geçilemez!");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında alanı boş geçilemez!");
+            RuleFor(x => x.WriterSurName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapınız!");
+            RuleFor(x => x.WriterSurName).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girmeyiniz!");
+        }
+    }
+}
--- MvcProje/Controllers/WriterController.cs
+++ MvcProje/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BussinessLayer.Concrete;
+using BussinessLayer.ValidationRules;
 using DataAccsessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,30 @@ namespace MvcProje.Controllers
         {
             var WriterValues = wm.GetList();
             return View(WriterValues);
+        }
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator writervalidator = new WriterValidator();
+            ValidationResult results = writervalidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
