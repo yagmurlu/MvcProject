5a8d14b260b8b9270d23315f7752d9ca0c10c81d
yagmurlu <mavibulut0106@gmail.com>
2021-05-21 23:20:18 +0300

    WriterEnd

--- BussinessLayer/ValidationRules/WriterValidator.cs
+++ BussinessLayer/ValidationRules/WriterValidator.cs
@@ -15,6 +15,7 @@ namespace BussinessLayer.ValidationRules
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçilemez!");
             RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadı boş geçilemez!");
             RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında alanı boş geçilemez!");
+            RuleFor(x => x.WriterTıtle).NotEmpty().WithMessage("Ünvan alanı boş geçilemez!");
             RuleFor(x => x.WriterSurName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapınız!");
             RuleFor(x => x.WriterSurName).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girmeyiniz!");
         }
---
+++ DataAccsessLayer/Migrations/202105212007372_mig_writertıtle_add.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccsessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_writertıtle_add : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Writers", "WriterTıtle", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Writers", "WriterTıtle");
+        }
+    }
+}
