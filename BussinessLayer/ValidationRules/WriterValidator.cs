8adb945ad57a10394842a34ae45c2b9423adeb09
yagmurlu <mavibulut0106@gmail.com>
2021-05-24 17:06:09 +0300

    New

--- BussinessLayer/ValidationRules/WriterValidator.cs
+++ BussinessLayer/ValidationRules/WriterValidator.cs
@@ -10,10 +10,12 @@ namespace BussinessLayer.ValidationRules
 {
     public class WriterValidator: AbstractValidator<Writer>
     {
-        public WriterValidator()//condtuctor method
+        public WriterValidator()//constuctor method
         {
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçilemez!");
             RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadı boş geçilemez!");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar maili boş geçilemez!");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Yazar şifre boş geçilemez!");
             RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında alanı boş geçilemez!");
             RuleFor(x => x.WriterTıtle).NotEmpty().WithMessage("Ünvan alanı boş geçilemez!");
             RuleFor(x => x.WriterSurName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapınız!");
