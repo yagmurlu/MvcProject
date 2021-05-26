7ffcdc8e7a41eadb201e55f97d0a0d5caa40f157
yagmurlu <mavibulut0106@gmail.com>
2021-05-27 02:35:15 +0300

    ContentController

---
+++ BussinessLayer/Abstract/IContentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Abstract
+{
+    public interface IContentService
+    {
+        List<Content> GetList();
+        void ContentAdd(Content content);
+        void ContentDelete(Content content);
+        void ContentUpdate(Content content);
+        Content GetById(int id);
+    }
+}
---
+++ BussinessLayer/Concrete/ContentManager.cs
@@ -0,0 +1,46 @@
+﻿using BussinessLayer.Abstract;
+using DataAccsessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class ContentManager : IContentService
+    {
+        IContentDal _contentDal;
+
+        public ContentManager(IContentDal contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public void ContentAdd(Content content)
+        {
+            _contentDal.Insert(content);
+        }
+
+        public void ContentDelete(Content content)
+        {
+            _contentDal.Delete(content);
+        }
+
+        public void ContentUpdate(Content content)
+        {
+            _contentDal.Update(content);
+        }
+
+        public Content GetById(int id)
+        {
+            return _contentDal.Get(x => x.ContentID == id);
+        }
+
+        public List<Content> GetList()
+        {
+            return _contentDal.List();
+        }
+    }
+}
