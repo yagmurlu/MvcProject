9b1d4d3242969641cde6273eb07c116851dcef2e
yagmurlu <mavibulut0106@gmail.com>
2021-06-29 23:10:48 +0300

    Message Class

---
+++ EntityLayer/Concrete/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Message
+    {
+        [Key]
+        public int MessageId { get; set; }
+
+        [StringLength(50)]
+        public string SenderMail { get; set; }//Gönderen
+        [StringLength(50)]
+        public string RecevierMail { get; set; }//Alıcı
+        [StringLength(100)]
+        public string Subject { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime MessageDate { get; set; }
+    }
+}
