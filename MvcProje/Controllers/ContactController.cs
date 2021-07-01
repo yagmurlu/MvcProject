cd180295cbd5e84baa2f3d51d36a23aa93fc7421
yagmurlu <mavibulut0106@gmail.com>
2021-07-02 02:10:24 +0300

    Count

--- MvcProje/Controllers/ContactController.cs
+++ MvcProje/Controllers/ContactController.cs
@@ -1,5 +1,6 @@
 ﻿using BussinessLayer.Concrete;
 using BussinessLayer.ValidationRules;
+using DataAccsessLayer.Concrete;
 using DataAccsessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ namespace MvcProje.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
         ContactManager cm = new ContactManager(new EfContactDal());
         ContactValidator cv = new ContactValidator();
         public ActionResult Index()
@@ -26,6 +28,23 @@ namespace MvcProje.Controllers
         }
         public PartialViewResult ContactPartial()
         {
+            var contact = cm.GetList().Count();
+            ViewBag.contact = contact;
+
+            var sendMail = messageManager.GetListSendBox().Count();
+            ViewBag.sendMail = sendMail;
+
+            var receiverMail = messageManager.GetListInbox().Count();
+            ViewBag.receiverMail = receiverMail;
+
+            //var draftMail = messageManager.GetListSendBox().Where(m => m.IsDraft == true).Count();
+            //ViewBag.draftMail = draftMail;
+
+            //var readMessage = messageManager.GetMessagesInbox().Where(m => m.IsRead == true).Count();
+            //ViewBag.readMessage = readMessage;
+
+            //var unreadMessage = messageManager.GetAllRead().Count();
+            //ViewBag.unreadMessage = unreadMessage;
             return PartialView();
         }
     }
