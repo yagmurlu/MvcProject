4fa7a6648feb7ffcaa3725276e2b8631d7b39b62
yagmurlu <mavibulut0106@gmail.com>
2021-09-03 17:40:16 +0300

    Session

--- BussinessLayer/Abstract/IContentService.cs
+++ BussinessLayer/Abstract/IContentService.cs
@@ -10,7 +10,7 @@ namespace BussinessLayer.Abstract
     public interface IContentService
     {
         List<Content> GetList();
-        List<Content> GetListByWriter();
+        List<Content> GetListByWriter(int id);
         List<Content> GetListByHeadingId(int id);
         void ContentAdd(Content content);
         void ContentDelete(Content content);
