fd23428f2a2ef925360e8c385092bbbdb80b2a07
yagmurlu <mavibulut0106@gmail.com>
2021-09-06 09:57:21 +0300

    Vitrin

---
+++ MvcProje/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using BussinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        public ActionResult Headings()
+        {
+            var headingList = hm.GetList();
+            return View(headingList);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
