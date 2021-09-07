da7fcd904e40c930296bf6bd89638ebe7018519a
yagmurlu <mavibulut0106@gmail.com>
2021-09-07 14:57:46 +0300

    Google Charts

---
+++ MvcProje/Controllers/ChartController.cs
@@ -0,0 +1,36 @@
+﻿using MvcProje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+        public List<CategoryClass>BlogList(){
+            List<CategoryClass> ct = new List<CategoryClass>();
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Yazılım",
+                CategoryCount=8
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Sağlık",
+                CategoryCount = 10
+            });
+            return ct;
+        }
+    }
+}
